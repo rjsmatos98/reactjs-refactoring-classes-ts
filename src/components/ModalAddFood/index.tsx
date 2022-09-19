558d25f04fe054bec2a0b139cdf06d948e5d3ef4
Raimundo Santos <rjsmatos98@gmail.com>
2022-09-19 00:58:22 -0300

    challenge done

---
+++ src/components/ModalAddFood/index.tsx
@@ -0,0 +1,62 @@
+import { useRef, useCallback } from 'react';
+import { FiCheckSquare } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from './styles';
+import { Modal } from '../Modal';
+import { Input } from '../Input';
+
+interface FoodPlate {
+  id: number;
+  name: string;
+  image: string;
+  price: string;
+  description: string;
+  available: boolean;
+}
+
+interface CreateFoodData {
+  name: string;
+  image: string;
+  price: string;
+  description: string;
+}
+
+
+interface ModalAddFoodProps{
+  isOpen: boolean;
+  setIsOpen: () => void;
+  handleAddFood: (food: Omit<FoodPlate, 'id' | 'available'>) => void;
+}
+
+export function ModalAddFood({isOpen, setIsOpen, handleAddFood}: ModalAddFoodProps){
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(
+    async (data: CreateFoodData) => {
+      const { name, image, price, description } = data;
+      handleAddFood({ name, image, price, description });
+      setIsOpen();
+    },
+    [handleAddFood, setIsOpen],
+  );
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <h1>Novo Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+        <button type="submit" data-testid="add-food-button">
+          <p className="text">Adicionar Prato</p>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+};
---
+++ src/components/ModalEditFood/index.tsx
@@ -0,0 +1,63 @@
+import { useRef, useCallback } from 'react';
+
+import { FiCheckSquare } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from './styles';
+import { Modal } from '../Modal';
+import { Input } from '../Input';
+
+interface FoodPlate {
+  id: number;
+  name: string;
+  image: string;
+  price: string;
+  description: string;
+  available: boolean;
+}
+
+interface ModalEditProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  handleUpdateFood: (food: Omit<FoodPlate, 'id' | 'available'>) => void;
+  editingFood: FoodPlate;
+}
+
+interface EditFoodData {
+  name: string;
+  image: string;
+  price: string;
+  description: string;
+}
+
+export function ModalEditFood({isOpen, setIsOpen, handleUpdateFood, editingFood}:ModalEditProps){
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(
+    async (data: EditFoodData) => {
+      handleUpdateFood(data);
+      setIsOpen();
+    },
+    [handleUpdateFood, setIsOpen],
+  );
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+};
